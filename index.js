5bd253aa1b001760d0b7f652df98a0fb06d7f890
Prathammehra7 <manshumehra7@gmail.com>
2022-09-24 21:18:31 +0530

    update

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ dbconnect();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }))
+const Paymentroutes = require("./Routes/Payment");
 app.use(logger("dev"));
 app.use(cors());
 app.use("/api", route);
 app.use("/movie", movie);
-
+app.use("/api/Payment",Paymentroutes)
 
 
 
